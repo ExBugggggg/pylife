1dcf1d6d70d22f6bc104365b8bd8efd54acb0866
Cookies <794942926@qq.com>
2022-08-23 22:04:36 +0800

    commit

--- pylife/settings.py
+++ pylife/settings.py
@@ -36,9 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'todolist',
-    'home'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
